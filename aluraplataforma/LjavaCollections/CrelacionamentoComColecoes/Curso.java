fe3f811ac13d41ee1cb15843af274a65071f1506
Jefferson Luis <jeflsantos@hotmail.com>
2022-06-15 15:42:13 -0300

    Java Collections
    Criando relacionamentos com coleções.

---
+++ aluraplataforma/LjavaCollections/CrelacionamentoComColecoes/Curso.java
@@ -0,0 +1,35 @@
+package aluraplataforma.LjavaCollections.CrelacionamentoComColecoes;
+
+import aluraplataforma.LjavaCollections.BlistasDeObjetos.Aula;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Curso {
+    private String nome;
+    private String instrutor;
+    private List<Aula> aulas = new LinkedList<Aula>();
+
+    public Curso(String nome, String instrutor) {
+        this.nome = nome;
+        this.instrutor = instrutor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getInstrutor() {
+        return instrutor;
+    }
+
+    public List<Aula> getAulas() {
+        return Collections.unmodifiableList(aulas);
+    }
+
+    public void adiciona(Aula aula){
+        this.aulas.add(aula);
+    }
+}
---
+++ aluraplataforma/LjavaCollections/CrelacionamentoComColecoes/TestaCurso.java
@@ -0,0 +1,19 @@
+package aluraplataforma.LjavaCollections.CrelacionamentoComColecoes;
+
+import aluraplataforma.LjavaCollections.BlistasDeObjetos.Aula;
+
+import java.util.List;
+
+public class TestaCurso {
+    public static void main(String[] args) {
+        Curso javaColecoes = new Curso("Dominando as coleções", "Paulo Silveira");
+
+        //javaColecoes.getAulas().add(new Aula("Trabalhando com ArrayList", 21));
+        javaColecoes.adiciona(new Aula("Trabalhando com ArrayList", 21));
+        javaColecoes.adiciona(new Aula("Criando uma aula", 20));
+        javaColecoes.adiciona(new Aula("Modelando com coleções", 22));
+
+        System.out.println(javaColecoes.getAulas());
+
+    }
+}
