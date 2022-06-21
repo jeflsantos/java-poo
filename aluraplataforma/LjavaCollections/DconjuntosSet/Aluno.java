473b79106cf97243cdc3d53556d1c07fa60c9b0d
Jefferson Luis <jeflsantos@hotmail.com>
2022-06-21 14:42:37 -0300

    Java Collection
    Equals e hashcode

--- aluraplataforma/LjavaCollections/DconjuntosSet/Aluno.java
+++ aluraplataforma/LjavaCollections/DconjuntosSet/Aluno.java
@@ -1,10 +1,15 @@
 package aluraplataforma.LjavaCollections.DconjuntosSet;
 
+import java.util.Objects;
+
 public class Aluno {
     private String nome;
     private int numeroMatricula;
 
     public Aluno(String nome, int numeroMatricula) {
+        if (nome == null){
+            throw new NullPointerException("Nome não pode ser null!");
+        }
         this.nome = nome;
         this.numeroMatricula = numeroMatricula;
     }
@@ -15,6 +20,17 @@ public class Aluno {
 
     public int getNumeroMatricula() {
         return numeroMatricula;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Aluno outro = (Aluno) obj;
+        return this.nome.equals(outro.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        return this.nome.hashCode();
     }
 
     @Override
--- aluraplataforma/LjavaCollections/DconjuntosSet/TestaCursoComAluno.java
+++ aluraplataforma/LjavaCollections/DconjuntosSet/TestaCursoComAluno.java
@@ -12,7 +12,7 @@ public class TestaCursoComAluno {
         javaColecoes.adiciona(new aluraplataforma.LjavaCollections.BlistasDeObjetos.Aula("Criando uma aula", 20));
         javaColecoes.adiciona(new Aula("Modelando com coleções", 22));
 
-        Aluno a1 = new Aluno("Rodrigo Turini", 1111);
+        Aluno a1 = new Aluno("Rodrigo Turini", 34672);
         Aluno a2 = new Aluno("Guilherme Silveira", 2222);
         Aluno a3 = new Aluno("Mauricio Anieche", 3333);
 
@@ -24,6 +24,20 @@ public class TestaCursoComAluno {
         javaColecoes.getAlunos().forEach(a -> {
             System.out.println(a);
         });
+
+        System.out.println("O aluno " +a1 + " está matriculado?");
+        System.out.println(javaColecoes.estaMatriculado(a1));
+
+        Aluno turini = new Aluno("Rodrigo Turini", 34672);
+        System.out.println("E esse turini, esta matriculado?");
+        System.out.println(a1.equals(turini));
+
+        System.out.println("O a1 é equals ao turini?");
+        System.out.println(a1.equals(turini));
+
+        //obrigatoriamente o seguinte é true:
+
+        System.out.println(a1.hashCode() == turini.hashCode());
 
     }
 }
