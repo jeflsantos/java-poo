938fb2718deeea7675a4092f2ca6fa9d63345251
Jefferson Luis <jeflsantos@hotmail.com>
2022-06-21 06:46:14 -0300

    Java Collection
    Conjunto Set

---
+++ aluraplataforma/LjavaCollections/DconjuntosSet/Curso.java
@@ -0,0 +1,47 @@
+package aluraplataforma.LjavaCollections.DconjuntosSet;
+
+import aluraplataforma.LjavaCollections.BlistasDeObjetos.Aula;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Curso {
+    private String nome;
+    private String instrutor;
+    private List<Aula> aulas = new LinkedList<Aula>();
+
+    public Curso(String nome, String instrutor) {
+        this.nome = nome;
+        this.instrutor = instrutor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getInstrutor() {
+        return instrutor;
+    }
+
+    public List<Aula> getAulas() {
+        return Collections.unmodifiableList(aulas);
+    }
+
+    public void adiciona(Aula aula) {
+        this.aulas.add(aula);
+    }
+
+    public int getTempoTotal() {
+        int tempoTotal = 0;
+        for (Aula aula : aulas) {
+            tempoTotal += aula.getTempo();
+        }
+        return tempoTotal;
+    }
+
+    @Override
+    public String toString() {
+        return "[Curso: " + nome + ", tempo total: " + this.getTempoTotal() + ", " + "aulas: " + this.aulas + "]";
+    }
+}
---
+++ aluraplataforma/LjavaCollections/DconjuntosSet/TestaAlunos.java
@@ -0,0 +1,23 @@
+package aluraplataforma.LjavaCollections.DconjuntosSet;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class TestaAlunos {
+    public static void main(String[] args) {
+        Set<String> alunos = new HashSet<>();
+        alunos.add("Rodrigo");
+        alunos.add("Alberto");
+        alunos.add("Nico");
+        alunos.add("Sergio");
+        alunos.add("Renan");
+        alunos.add("Mauricio");
+
+        alunos.forEach(aluno -> {
+            System.out.println(aluno);
+        });
+
+
+
+    }
+}
